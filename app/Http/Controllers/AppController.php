155084cb64c6ac0fa10a013b565bdcb2f7210cdd
phpeditor <phpeditor96@gmail.com>
2022-08-23 16:58:48 +0300

    commit 2

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+class AppController extends Controller
+{
+
+}
